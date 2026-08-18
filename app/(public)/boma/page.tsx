cc9517f7b7070a781bacc48c40de5902fdd67049
Edd <njugunakirika@gmail.com>
2026-08-18 20:54:40 +0300

    Boma cropped picture

--- app/(public)/boma/page.tsx
+++ app/(public)/boma/page.tsx
@@ -10,7 +10,7 @@ export default function BomaPage() {
       {/* ================= HERO SECTION ================= */}
       <section className="relative w-full h-[500px] flex items-center justify-center text-center">
         <Image
-          src="/boma-land.webp"
+          src="/boma-land.jpg"
           alt="Boma Village Land"
           fill
           priority
